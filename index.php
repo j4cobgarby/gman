7655cae5aede3a61620442510cbbdf205135706e
Jacob Garby <j.garby@icloud.com>
2017-06-27 20:03:33 +0100

    Set up some php stuff

--- index.php
+++ index.php
@@ -5,21 +5,37 @@
     <title>GMan</title>
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css?family=Fira+Mono|Montserrat:400,500" rel="stylesheet">
+    <?php
+      $arrayName = array('README.md', 'README.txt', 'README');
+    ?>
   </head>
   <body>
     <div class="top">
-      <h1>gMan</h1>
-      <h4>The most aesthetic GitHub README viewer.</h4>
+      <h1 class="title">gMan</h1>
+      <h4 class="subtitle">The most aesthetic GitHub README viewer.</h4>
     </div>
     <div class="input">
-      <form action="" method="post">
+      <form action="" method="get">
         <label>github.com/</label>
-        <input name="path" spellcheck=false type="text" placeholder="username/repo">
+        <input name="username" spellcheck=false type="text" placeholder="username">
+        <input name="repo" spellcheck=false type="text" placeholder="repo">
         <input type="submit" value="Go!">
       </form>
     </div>
     <div class="result">
-      
+      <div class="inner">
+        <h1 class="filename">
+          <?php
+            if (!isset($_GET["username"]) || !isset($_GET["repo"])) {
+              echo "You haven't specified a repo yet.";
+            } else {
+
+            }
+          ?>
+        </h1>
+        <div class="file">
+        </div>
+      </div>
     </div>
   </body>
 </html>
