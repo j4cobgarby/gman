3c50482261141f91bd278b9ff1f62ec340f85b72
Jacob Garby <j.garby@icloud.com>
2017-06-27 20:30:42 +0100

    Got getting of readme workgin

--- index.php
+++ index.php
@@ -6,7 +6,44 @@
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css?family=Fira+Mono|Montserrat:400,500" rel="stylesheet">
     <?php
-      $arrayName = array('README.md', 'README.txt', 'README');
+      $readmes = array('README.md', 'README.txt', 'README');
+
+      function URLExists($url) {
+        $c = curl_init($url);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        $source = curl_exec($c);
+        $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+        curl_close($c);
+
+        if ($status != 404) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      function getReadmeName($username, $repo) {
+        global $readmes;
+        $done = false;
+        foreach ($readmes as $key => $file) {
+          if (URLExists("https://raw.githubusercontent.com/".$username."/".$repo."/master/".$file)) {
+            $done = true;
+            return "https://raw.githubusercontent.com/".$username."/".$repo."/master/".$file;
+          }
+        }
+        if ($done == false) {
+          return 0;
+        }
+      }
+
+      function getSource($url) {
+        $c = curl_init($url);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        $source = curl_exec($c);
+        curl_close($c);
+
+        return $source;
+      }
     ?>
   </head>
   <body>
@@ -17,23 +54,27 @@
     <div class="input">
       <form action="" method="get">
         <label>github.com/</label>
-        <input name="username" spellcheck=false type="text" placeholder="username">
-        <input name="repo" spellcheck=false type="text" placeholder="repo">
+        <input name="username" spellcheck=false type="text" placeholder="username" <?php echo (isset($_GET["username"]) ? 'value='.$_GET["username"] : '') ?>>
+        <input name="repo" spellcheck=false type="text" placeholder="repo" <?php echo (isset($_GET["repo"]) ? 'value='.$_GET["repo"] : '') ?>>
         <input type="submit" value="Go!">
       </form>
     </div>
     <div class="result">
       <div class="inner">
-        <h1 class="filename">
-          <?php
-            if (!isset($_GET["username"]) || !isset($_GET["repo"])) {
-              echo "You haven't specified a repo yet.";
-            } else {
-
-            }
-          ?>
-        </h1>
         <div class="file">
+          <pre>
+<?php
+  if (!isset($_GET["username"]) || !isset($_GET["repo"])) {
+    echo "You haven't specified a repo yet.";
+  } else {
+    $readmeURL = getReadmeName($_GET["username"], $_GET["repo"]);
+    echo getSource($readmeURL);
+    if ($readmeURL == 0) {
+      echo "README not found.";
+    }
+  }
+?>
+          </pre>
         </div>
       </div>
     </div>
